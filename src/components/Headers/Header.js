1948078da60695a2bc27102cd27d47c54aab7268
Mridul2021 <119924746+Mridul2021@users.noreply.github.com>
2023-11-28 21:34:23 +0530

    Update Header.js

--- src/components/Headers/Header.js
+++ src/components/Headers/Header.js
@@ -7,11 +7,6 @@ const Header = () => {
   return (
     <AppBar position="fixed" style={{ backgroundColor: 'white' }}>
       <Toolbar>
-        <img
-          src="https://assets-global.website-files.com/63ca2acc6352c221abe583d0/63caaeb042a0775de3e30f22_dyte-logo.svg"
-          alt="Dyte Logo"
-          style={{ height: '40px', marginRight: '10px' }}
-        />
         <Typography variant="h6" color='black'>
         Log Ingestor
         </Typography>
